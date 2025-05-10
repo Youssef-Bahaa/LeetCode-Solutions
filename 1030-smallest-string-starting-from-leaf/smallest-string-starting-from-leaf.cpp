12cb2c6524e075a7a0d87324e8a8f0fd3782382e
Youssef-Bahaa <162743130+Youssef-Bahaa@users.noreply.github.com>
2025-05-10 11:17:22 +0300

    Time: 0 ms (100.00%) | Memory: 23.5 MB (22.08%) - LeetSync

--- 1030-smallest-string-starting-from-leaf/smallest-string-starting-from-leaf.cpp
+++ 1030-smallest-string-starting-from-leaf/smallest-string-starting-from-leaf.cpp
@@ -6,29 +6,34 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
     vector<string> v;
-    bool is_leaf(TreeNode* root){
-        return !root->left and !root->right;
-    }
-    void dfs(TreeNode* root , string s){
-        
-        if(is_leaf(root)){v.push_back(   char(root->val + 'a') + s );return;}
+    bool is_leaf(TreeNode* root) { return !root->left and !root->right; }
+    void dfs(TreeNode* root, string s) {
 
-        if(root->left)
-            dfs(root->left ,   char(root->val + 'a') + s);
+        if (is_leaf(root)) {
+            v.push_back(char(root->val + 'a') + s);
+            return;
+        }
 
-         if(root->right)
-            dfs(root->right ,  char(root->val + 'a') + s );
+        if (root->left)
+            dfs(root->left, char(root->val + 'a') + s);
+
+        if (root->right)
+            dfs(root->right, char(root->val + 'a') + s);
     }
 
     string smallestFromLeaf(TreeNode* root) {
-        dfs(root,"");
-        string ans = *min_element(v.begin(), v.end());
+        dfs(root, "");
+        string ans = v[0];
+        for (int i = 1; i < v.size(); ++i) 
+            ans = min(ans , v[i]);
+            
         return ans;
     }
 };
