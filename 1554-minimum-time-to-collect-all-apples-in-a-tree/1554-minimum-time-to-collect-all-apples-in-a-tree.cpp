d597174291eb0ed76236a0aae679b68739043029
Youssef-Bahaa <162743130+Youssef-Bahaa@users.noreply.github.com>
2025-05-27 00:55:14 +0300

    Time: 59 ms (60.09%), Space: 69.9 MB (50.99%) - LeetHub

--- 1554-minimum-time-to-collect-all-apples-in-a-tree/1554-minimum-time-to-collect-all-apples-in-a-tree.cpp
+++ 1554-minimum-time-to-collect-all-apples-in-a-tree/1554-minimum-time-to-collect-all-apples-in-a-tree.cpp
@@ -1,31 +1,44 @@
 class Solution {
 public:
     typedef vector<vector<int>> GRAPH;
+    int time = 0;
 
-    int dfs(GRAPH& graph, vector<bool>& hasApple, int cur,int parent) {
-        int ans = 0 ;
+    bool routehasapples(GRAPH& graph, int parent, int start,vector<bool>& hasApple) {
+        if (hasApple[start])
+            return true;
 
-        for(auto i : graph[cur]){
-            if(i==parent)continue;
-            ans += dfs(graph , hasApple , i , cur);
+        for (auto i : graph[start]) {
+            if (i == parent)
+                continue;
+            if (routehasapples(graph, start, i, hasApple))
+                return true;
         }
-        if( ans > 0 or hasApple[cur])
-            ans += 2;
 
-        return ans;
+        return false;
     }
+    void dfs(GRAPH& graph, vector<bool>& hasApple, int start,int parent) {
+        for (auto neighbour : graph[start]) {
 
+            if (neighbour == parent)
+                continue;
 
+            if (routehasapples(graph, start, neighbour, hasApple)) {
+                time += 2;
+                dfs(graph, hasApple, neighbour, start);
+            }
+        }
+    }
     int minTime(int n, vector<vector<int>>& edges, vector<bool>& hasApple) {
         GRAPH Graph(n);
+        vector<bool> vis(n, 0);
 
         for (auto i : edges) {
             Graph[i[0]].push_back(i[1]);
             Graph[i[1]].push_back(i[0]);
         }
 
-        int ans =  dfs(Graph, hasApple, 0, -1);
-        if(ans)return ans - 2;
-        return 0 ;
+        dfs(Graph, hasApple, 0, -1);
+
+        return time;
     }
 };
