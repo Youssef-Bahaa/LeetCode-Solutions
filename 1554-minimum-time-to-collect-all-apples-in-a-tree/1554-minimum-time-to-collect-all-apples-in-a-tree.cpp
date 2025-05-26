2196f281c4b4f46ad2badb0846a34e975607cb6b
Youssef-Bahaa <162743130+Youssef-Bahaa@users.noreply.github.com>
2025-05-27 00:19:44 +0300

    Time: 63 ms (54.83%), Space: 69.9 MB (54.94%) - LeetHub

--- 1554-minimum-time-to-collect-all-apples-in-a-tree/1554-minimum-time-to-collect-all-apples-in-a-tree.cpp
+++ 1554-minimum-time-to-collect-all-apples-in-a-tree/1554-minimum-time-to-collect-all-apples-in-a-tree.cpp
@@ -1,45 +1,31 @@
 class Solution {
 public:
     typedef vector<vector<int>> GRAPH;
-    int time = 0;
 
-    bool routehasapples(GRAPH& graph, vector<bool>& vis, int start,vector<bool>& hasApple) {
+    int dfs(GRAPH& graph, vector<bool>& hasApple, int cur,int parent) {
+        int ans = 0 ;
 
-
-        if (hasApple[start])
-            return true;
-
-        vis[start] = 1;
-
-        for (auto i : graph[start]) {
-            if (!vis[i] and routehasapples(graph, vis, i, hasApple))
-                return true;
+        for(auto i : graph[cur]){
+            if(i==parent)continue;
+            ans += dfs(graph , hasApple , i , cur);
         }
+        if( ans > 0 or hasApple[cur])
+            ans += 2;
 
-        return false;
+        return ans;
     }
-    void dfs(GRAPH& graph, vector<bool>& hasApple, int start,vector<bool>& vis) {
-        vis[start] = 1;
-        for (auto neighbour : graph[start])
-            if (!vis[neighbour]) {
-                vector<bool> tempvis(vis);
-                if (routehasapples(graph, tempvis, neighbour, hasApple)) {
-                    time += 2;
-                    dfs(graph, hasApple, neighbour, vis);
-                }
-            }
-    }
+
+
     int minTime(int n, vector<vector<int>>& edges, vector<bool>& hasApple) {
         GRAPH Graph(n);
-        vector<bool> vis(n, 0);
 
         for (auto i : edges) {
             Graph[i[0]].push_back(i[1]);
             Graph[i[1]].push_back(i[0]);
         }
 
-        dfs(Graph, hasApple, 0, vis);
-
-        return time;
+        int ans =  dfs(Graph, hasApple, 0, -1);
+        if(ans)return ans - 2;
+        return 0 ;
     }
 };
