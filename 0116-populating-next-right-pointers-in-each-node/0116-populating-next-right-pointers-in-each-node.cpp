7bb2498a351a8643af5477f721ecccadc5be2853
Youssef-Bahaa <162743130+Youssef-Bahaa@users.noreply.github.com>
2025-06-02 23:58:44 +0300

    Time: 20 ms (7.58%), Space: 18.8 MB (98.69%) - LeetHub

--- 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.cpp
+++ 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.cpp
@@ -18,32 +18,17 @@ public:
 
 class Solution {
 public:
-    vector<Node*> nodes;
-    void bfs(Node* root){
-        queue<Node*> q;
-        q.push(root);
+    void dfs(Node* root){
+        if(!root)return;
 
-        while(!q.empty()){
-            int sz = q.size();
-            while(sz--){
-                Node* temp = q.front();
-                q.pop();
+        if(root->left)root->left->next = root->right;
+        if(root->right and root->next)root->right->next = root->next->left;
 
-                if(temp->left)q.push(temp->left);
-                if(temp->right)q.push(temp->right);
-
-                nodes.push_back(temp);
-            }
-            nodes.push_back(nullptr);
-        }
+        dfs(root->left);
+        dfs(root->right);
     }
     Node* connect(Node* root) {
-        if(!root)return {};
-        bfs(root);
-        for(int i = 0; i < nodes.size() ; i++){
-            if(nodes[i])
-                nodes[i]->next = nodes[i + 1];
-        }
-        return nodes[0];
+        dfs(root);
+        return root;
     }
 };
