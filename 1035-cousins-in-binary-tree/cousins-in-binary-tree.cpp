28fd89161eb639d2603616c3bb02b025e46be770
Youssef-Bahaa <162743130+Youssef-Bahaa@users.noreply.github.com>
2025-06-09 09:09:35 +0300

    Time: 0 ms (100.00%) | Memory: 14 MB (78.79%) - LeetSync

--- 1035-cousins-in-binary-tree/cousins-in-binary-tree.cpp
+++ 1035-cousins-in-binary-tree/cousins-in-binary-tree.cpp
@@ -6,41 +6,50 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
- TreeNode* find_parent(TreeNode* cur,int val) {
-     if (!cur or cur->val==val)return 0;
-     if (cur->left) 
-         if (cur->left->val == val)return cur;
-     
-     if (cur->right) 
-         if (cur->right->val == val)return cur;
-     
-     TreeNode* leftparent = find_parent(cur->left, val);;
-     if (leftparent)return leftparent;
-    
-     return find_parent(cur->right, val);
- }
+    TreeNode* find_parent(TreeNode* cur, int val) {
+        if (!cur or cur->val == val)
+            return 0;
+        if (cur->left)
+            if (cur->left->val == val)
+                return cur;
 
-  int depth(TreeNode* c, int value,int cur=0) {
-     
-     if (!c)return 0;
-     if (c->val == value)return cur;
+        if (cur->right)
+            if (cur->right->val == val)
+                return cur;
 
-     int l = depth(c->left, value, cur + 1);
-     if(l)
-       return l;
+        TreeNode* leftparent = find_parent(cur->left, val);
+        ;
+        if (leftparent)
+            return leftparent;
 
-     return depth(c->right, value, cur + 1);
-}
+        return find_parent(cur->right, val);
+    }
+
+    int depth(TreeNode* c, int value, int cur = 0) {
+
+        if (!c)
+            return 0;
+        if (c->val == value)
+            return cur;
+
+        int l = depth(c->left, value, cur + 1);
+        if (l)
+            return l;
+
+        return depth(c->right, value, cur + 1);
+    }
 
     bool isCousins(TreeNode* root, int x, int y) {
-        if(depth(root,x)!=depth(root,y))
+        if (depth(root, x) != depth(root, y))
             return false;
-        if(find_parent(root,x)==find_parent(root,y))return false;
+        if (find_parent(root, x) == find_parent(root, y))
+            return false;
         return true;
     }
 };
