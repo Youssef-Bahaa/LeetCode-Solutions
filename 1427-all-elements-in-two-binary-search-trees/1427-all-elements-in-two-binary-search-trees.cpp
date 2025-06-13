def5db2fc1a5ea564324830527f887b4f686ea87
Youssef-Bahaa <162743130+Youssef-Bahaa@users.noreply.github.com>
2025-06-13 15:35:35 +0300

    Time: 4 ms (75.54%), Space: 89.4 MB (6.25%) - LeetHub

--- 1427-all-elements-in-two-binary-search-trees/1427-all-elements-in-two-binary-search-trees.cpp
+++ 1427-all-elements-in-two-binary-search-trees/1427-all-elements-in-two-binary-search-trees.cpp
@@ -11,17 +11,31 @@
  */
 class Solution {
 public:
-    vector<int> ans;
-    void dfs(TreeNode* root1){
+    vector<int> m1,m2,ans;
+    void dfs(TreeNode* root1,vector<int>& v){
         if(!root1)return;
-        dfs(root1->left);
-        ans.push_back(root1->val);
-        dfs(root1->right);
+        dfs(root1->left ,v);
+        v.push_back(root1->val);
+        dfs(root1->right,v);
     }
     vector<int> getAllElements(TreeNode* root1, TreeNode* root2) {
-        dfs(root1);
-        dfs(root2);
-        sort(ans.begin(),ans.end());
+        dfs(root1,m1);
+        dfs(root2,m2);
+        
+        int i = 0, j = 0;
+
+        while(i < m1.size() and j < m2.size()){
+            if(m1[i] <= m2[j] )
+                ans.push_back(m1[i++]);
+            
+            else 
+                ans.push_back(m2[j++]);
+            
+        }
+
+        while(i < m1.size())ans.push_back(m1[i++]);
+        while(j < m2.size())ans.push_back(m2[j++]);
+
         return ans;
     }
 };
