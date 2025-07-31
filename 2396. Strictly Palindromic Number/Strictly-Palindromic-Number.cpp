08de77742c1220d85f16d5afc57b74ea0f881491
Youssef-Bahaa <162743130+Youssef-Bahaa@users.noreply.github.com>
2025-07-31 18:33:19 +0300

    [2396] [Time Beats: 100.00%] [Memory Beats: 90.36%] - LeetPush

--- 2396. Strictly Palindromic Number/Strictly-Palindromic-Number.cpp
+++ 2396. Strictly Palindromic Number/Strictly-Palindromic-Number.cpp
@@ -1,28 +1,6 @@
 class Solution {
 public:
-    string b(int n, int base) {
-        string s = "";
-        while (n) {
-            s += char(n % base + '0');
-            n /= base;
-        }
-        reverse(s.begin(), s.end());
-        return s;
-    }
-    bool pal(string s) {
-        for (int i = 0; i < s.size() / 2; i++) {
-            if (s[i] != s[s.size() - i - 1])
-                return false;
-        }
-        return true;
-    }
     bool isStrictlyPalindromic(int n) {
-
-        for (int i = 2; i <= n - 2; i++) {
-            string s = b(n, i);
-            if (pal(s) == 0)
-                return false;
-        }
-        return true;
+        return false;
     }
 };
