6a5d6635fc71c46b182d75fc88d1da582a0659b0
Youssef-Bahaa <162743130+Youssef-Bahaa@users.noreply.github.com>
2025-08-02 18:53:57 +0300

    [2610] [Time Beats: 100.00%] [Memory Beats: 87.19%] - LeetPush

--- 2610. Convert an Array Into a 2D Array With Conditions/Convert-an-Array-Into-a-2D-Array-With-Conditions.cpp
+++ 2610. Convert an Array Into a 2D Array With Conditions/Convert-an-Array-Into-a-2D-Array-With-Conditions.cpp
@@ -2,20 +2,17 @@ class Solution {
 public:
     vector<vector<int>> findMatrix(vector<int>& nums) {
         unordered_map<int, int> mp;
-        int maxi = INT_MIN;
-        for (auto i : nums) {
-            mp[i]++;
-        }
-        for (auto i : mp)
-            maxi = max(maxi, i.second);
+        int maxi = 0;
+        for (auto i : nums)
+            maxi = max(maxi, ++mp[i]);
 
         vector<vector<int>> ans(maxi);
 
         for (int i = 0; i < maxi; i++) {
-            for (auto j : mp) {
+            for (auto& j : mp) {
                 if (j.second) {
                     ans[i].push_back(j.first);
-                    mp[j.first]--;
+                    j.second--;
                 }
             }
         }
