e8f918bb0e018b2eb73d0b82099f2f94cbe8285a
Youssef-Bahaa <162743130+Youssef-Bahaa@users.noreply.github.com>
2025-08-09 17:19:08 +0300

    [231] [Time Beats: 100.00%] [Memory Beats: 17.71%] - LeetPush

---
+++ 231. Power of Two/Power-of-Two.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        int i = 0;
+        while (true) {
+            if (pow(2, i++) == n)
+                return true;
+            else if (pow(2, i) > n)
+                break;
+        }
+        return false;
+    }
+};
