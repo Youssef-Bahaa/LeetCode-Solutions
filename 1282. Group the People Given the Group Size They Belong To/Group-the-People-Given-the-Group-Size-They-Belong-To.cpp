c52c8f51067ac66cc3858bc9452b7fa6e51695ba
Youssef-Bahaa <162743130+Youssef-Bahaa@users.noreply.github.com>
2025-09-12 19:28:00 +0300

    [1282] [Time Beats: 72.50%] [Memory Beats: 82.25%] - LeetPush

--- 1282. Group the People Given the Group Size They Belong To/Group-the-People-Given-the-Group-Size-They-Belong-To.cpp
+++ 1282. Group the People Given the Group Size They Belong To/Group-the-People-Given-the-Group-Size-They-Belong-To.cpp
@@ -3,22 +3,13 @@ public:
     vector<vector<int>> groupThePeople(vector<int>& groupSizes) {
         vector<vector<int>> ans;
         unordered_map<int, vector<int>> mp;
-        for (int i = 0; i < groupSizes.size(); i++)
+        for (int i = 0; i < groupSizes.size(); i++) {
             mp[groupSizes[i]].push_back(i);
-
-        for (auto i : mp) {
-            int val = i.first;
-            int sz = i.second.size();
-            vector<int> temp;
-            for (int j = 0; j < sz; j++) {
-                if (j != 0 and j % val == 0) {
-                    ans.push_back(temp);
-                    temp.clear();
-                }
-                temp.push_back(i.second[j]);
+            int sz = groupSizes[i];
+            if (mp[groupSizes[i]].size() == sz) {
+                ans.push_back(mp[groupSizes[i]]);
+                mp[groupSizes[i]].clear();
             }
-            if (!temp.empty())
-                ans.push_back(temp);
         }
 
         return ans;
