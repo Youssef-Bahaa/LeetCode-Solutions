e1cc600c2597002a07a20f5e9b03693009ba9c46
Youssef-Bahaa <162743130+Youssef-Bahaa@users.noreply.github.com>
2025-09-23 21:05:49 +0300

    [165] [Time Beats: 100.00%] [Memory Beats: 30.92%] - LeetPush

--- 165. Compare Version Numbers/Compare-Version-Numbers.cpp
+++ 165. Compare Version Numbers/Compare-Version-Numbers.cpp
@@ -1,40 +1,27 @@
 class Solution {
 public:
     int compareVersion(string version1, string version2) {
-        string collect = "";
-        version1 += ".";
-        version2 += ".";
         vector<int> num1;
         vector<int> num2;
-        for (auto i : version1) {
-            if (i == '.') {
-                num1.push_back(stoi(collect));
-                collect = "";
-            } else
-                collect += i;
+        parser(num1, version1);
+        parser(num2, version2);
+        for (int i = 0; i < max(num1.size(), num2.size()); i++) {
+            int nm1 = i < num1.size() ? num1[i] : 0;
+            int nm2 = i < num2.size() ? num2[i] : 0;
+            if (nm1 < nm2)return -1;
+            if (nm1 > nm2)return 1;
         }
-        collect = "";
-        for (auto i : version2) {
-            if (i == '.') {
-                num2.push_back(stoi(collect));
-                collect = "";
-            } else
-                collect += i;
-        }
-        for(auto i :num1 )cout<<i <<" ";
-
-        while (num1.size() < num2.size())
-            num1.push_back(0);
-        while (num1.size() > num2.size())
-            num2.push_back(0);
-
-        for (int i = 0; i < num1.size(); i++) {
-            if (num1[i] < num2[i])
-                return -1;
-            if (num1[i] > num2[i])
-                return 1;
-        }
-
         return 0;
+    }
+    void parser(vector<int>& num, string& s) {
+        s += ".";
+        string collect = "";
+        for (auto i : s) {
+            if (i == '.') {
+                num.push_back(stoi(collect));
+                collect = "";
+            } else
+                collect += i;
+        }
     }
 };
