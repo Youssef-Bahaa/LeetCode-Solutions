5347c7a980607ea367d54882abc9d17ec0fa0c05
Youssef-Bahaa <162743130+Youssef-Bahaa@users.noreply.github.com>
2025-09-26 00:20:20 +0300

    [1486] [Time Beats: 100.00%] [Memory Beats: 98.11%] - LeetPush

--- 1486. XOR Operation in an Array/XOR-Operation-in-an-Array.cpp
+++ 1486. XOR Operation in an Array/XOR-Operation-in-an-Array.cpp
@@ -1,12 +1,9 @@
 class Solution {
 public:
     int xorOperation(int n, int start) {
-        vector<int> arr;
         int ans = 0 ;
         for(int i = 0 ; i < n ; i++)
-            arr.push_back(start + 2 * i);
-        for (int i = 0 ; i < arr.size(); i++)
-            ans = ans ^ arr[i];
+            ans = ans ^ start + 2 * i;
         return ans;        
     }
 };
