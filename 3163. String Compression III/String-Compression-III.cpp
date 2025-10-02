151ff300d2f53884656dc258129eec89a2a5654a
Youssef-Bahaa <162743130+Youssef-Bahaa@users.noreply.github.com>
2025-10-02 23:54:13 +0300

    [3163] [Time Beats: 92.25%] [Memory Beats: 5.12%] - LeetPush

---
+++ 3163. String Compression III/String-Compression-III.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string compressedString(string word) {
+        word += " ";
+        string ans = "";
+        int cnt = 1;
+        for (int i = 1; i < word.size(); i++) {
+            if (word[i] == word[i - 1] and cnt < 9) 
+                cnt++;
+            else {
+                ans.push_back(cnt +'0');
+                ans.push_back(word[i-1]);
+                cnt = 1;
+            }
+        }
+        return ans;
+    }
+};
