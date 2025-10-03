264cd590063d55cb8167b97e7a96ab39564e9c8c
Youssef-Bahaa <162743130+Youssef-Bahaa@users.noreply.github.com>
2025-10-03 11:33:51 +0300

    [860] [Time Beats: 100.00%] [Memory Beats: 38.94%] - LeetPush

---
+++ 860. Lemonade Change/Lemonade-Change.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+        int five = 0, ten = 0;
+        for (auto i : bills) {
+            if (i == 5)five++;
+            else if (i == 10) {
+                if (!five)return false;
+                five--;
+                ten++;
+            } else {
+                if (five > 0 and ten > 0) {
+                    five--;
+                    ten--;
+                } else if (five >= 3) {
+                    five -= 3;
+                }
+                else return false;
+            }
+        }
+        return true;
+    }
+};
