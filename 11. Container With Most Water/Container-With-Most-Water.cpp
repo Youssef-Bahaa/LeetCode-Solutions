e7d4ad762f31ca4343f38380fa83c6eee8bafb0e
Youssef-Bahaa <162743130+Youssef-Bahaa@users.noreply.github.com>
2025-10-04 10:47:22 +0300

    [11] [Time Beats: 100.00%] [Memory Beats: 97.05%] - LeetPush

---
+++ 11. Container With Most Water/Container-With-Most-Water.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int area=0;
+        int l=0,r=height.size()-1;
+        while(l<r){
+            area = max(area,(r-l) *  min(height[l],height[r]));
+            if(height[r]<=height[l])r--;
+            else l++;
+        }
+        return area;
+    }
+};
