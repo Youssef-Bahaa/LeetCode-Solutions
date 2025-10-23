179573d80920741f74f2547a2a4138a5dcb83f0a
Youssef-Bahaa <162743130+Youssef-Bahaa@users.noreply.github.com>
2025-10-23 15:35:02 +0300

    [3461] [Time Beats: 100.00%] [Memory Beats: 81.14%] - LeetPush

---
+++ 3461. Check If Digits Are Equal in String After Operations I/Check-If-Digits-Are-Equal-in-String-After-Operations-I.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool hasSameDigits(string s) {
+        int n = s.size();
+        vector<int> nums;
+        for (auto i : s)nums.push_back(i - '0');
+        while (n > 2) {
+            for (int i = 0; i < n - 1; i++)nums[i] = (nums[i] + nums[i + 1]) % 10;
+            n--;
+        }
+        return nums[0] == nums[1];
+    }
+};
