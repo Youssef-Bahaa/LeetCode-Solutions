98cd1a29f557008769f16251722c5de0f4f2a88f
Youssef-Bahaa <162743130+Youssef-Bahaa@users.noreply.github.com>
2025-11-01 10:18:46 +0200

    [3217] [Time Beats: 82.16%] [Memory Beats: 76.09%] - LeetPush

--- 3217. Delete Nodes From Linked List Present in Array/Delete-Nodes-From-Linked-List-Present-in-Array.cpp
+++ 3217. Delete Nodes From Linked List Present in Array/Delete-Nodes-From-Linked-List-Present-in-Array.cpp
@@ -12,21 +12,17 @@ class Solution {
 public:
     ListNode* modifiedList(vector<int>& nums, ListNode* head) {
         unordered_set<int> mp(nums.begin(), nums.end());
-
+        
         ListNode* dummy = new ListNode(0);
         dummy->next = head;
         ListNode* prev = dummy;
-
-        while (head) {
-            if (mp.count(head->val)) {
+        while(head){
+            if(mp.count(head->val)){
                 prev->next = head->next;
-                head = head->next;
-            } else {
-                prev = head;
-                head = head->next;
             }
+            else prev = head;
+            head = head->next;
         }
-
         return dummy->next;
     }
 };
