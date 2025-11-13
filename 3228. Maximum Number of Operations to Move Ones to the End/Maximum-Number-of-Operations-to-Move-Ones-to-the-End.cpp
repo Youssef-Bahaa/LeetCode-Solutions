1a2947bcad5692b9da8901b98f54936fb74ebc76
Youssef-Bahaa <162743130+Youssef-Bahaa@users.noreply.github.com>
2025-11-13 16:21:09 +0200

    [3228] [Time Beats: 83.33%] [Memory Beats: 85.14%] - LeetPush

--- 3228. Maximum Number of Operations to Move Ones to the End/Maximum-Number-of-Operations-to-Move-Ones-to-the-End.cpp
+++ 3228. Maximum Number of Operations to Move Ones to the End/Maximum-Number-of-Operations-to-Move-Ones-to-the-End.cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
     int maxOperations(string s) {
-        int one_cnt = 0 , ans = 0;
-        for(int i = 0 ; i < s.size(); i++){
-            if(s[i] == '1')one_cnt++;
+        int one_cnt = 0, ans = 0;
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == '1')
+                one_cnt++;
             else {
                 ans += one_cnt;
-                while(i < s.size() and s[i] == '0')i++;
-                i--;
+                while (i + 1 < s.size() and s[i + 1] == '0')
+                    i++;
             }
         }
         return ans;
