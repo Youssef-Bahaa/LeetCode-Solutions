accd0e7a11d8725c7edf7639495a26e115d33fe5
Youssef-Bahaa <162743130+Youssef-Bahaa@users.noreply.github.com>
2025-11-20 23:46:12 +0200

    [35] [Time Beats: 100.00%] [Memory Beats: 41.83%] - LeetPush

---
+++ 35. Search Insert Position/Search-Insert-Position.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int l = 0 , r = nums.size() - 1,idx = 0;
+        while(l <= r){
+            int mid = (l + r ) / 2;
+            if(nums[mid] < target)l = mid + 1;
+            else if (nums[mid] > target)r = mid - 1;
+            else return mid;
+            idx = l;
+        }
+        return idx;
+    }
+};
